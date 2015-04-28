8f2e9bc48ef901a460f61b9a0b59a58620aa7679
Serge Fomin <serge.fo@gmail.com>
2015-04-27 23:30:49 -0700

    Testing travis ci builds.

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 clear
 
-#export PKG_DIR=/usr/local
-#export LD_LIBRARY_PATH=$PKG_DIR/lib64:$PKG_DIR/lib:$LD_LIBRARY_PATH
-#export LD_RUN_PATH=$LD_LIBRARY_PATH
-#export PKG_CONFIG_PATH=$PKG_DIR/lib64/pkgconfig:$PKG_CONFIG_PATH
-#export MONO_GAC_PREFIX=${PKG_DIR}
-#export MONO_PATH=${PKG_DIR}/lib/mono/4.5:${PKG_DIR}/lib/mono/4.0:${PKG_DIR}/lib/mono/3.5:${PKG_DIR}/lib/mono/2.0:${PKG_DIR}/lib/mono/compat-2.0
-#export MONO_CONFIG=${PKG_DIR}/etc/mono/config
-#export MONO_CFG_DIR=${PKG_DIR}/etc
-#export C_INCLUDE_PATH=${PKG_DIR}/include
-#export ACLOCAL_PATH=${PKG_DIR}/share/aclocal
-#export MONO_REGISTRY_PATH=~/.mono/registry
+set -ev
+
+if [ "${TRAVIS}" != "true" ]; then
+    export PKG_DIR=/usr/local
+    export LD_LIBRARY_PATH=$PKG_DIR/lib64:$PKG_DIR/lib:$LD_LIBRARY_PATH
+    export LD_RUN_PATH=$LD_LIBRARY_PATH
+    export PKG_CONFIG_PATH=$PKG_DIR/lib64/pkgconfig:$PKG_CONFIG_PATH
+    export MONO_GAC_PREFIX=${PKG_DIR}
+    export MONO_PATH=${PKG_DIR}/lib/mono/4.5:${PKG_DIR}/lib/mono/4.0:${PKG_DIR}/lib/mono/3.5:${PKG_DIR}/lib/mono/2.0:${PKG_DIR}/lib/mono/compat-2.0:${PWD}/build/FAKE/tools
+    export MONO_CONFIG=${PKG_DIR}/etc/mono/config
+    export MONO_CFG_DIR=${PKG_DIR}/etc
+    export C_INCLUDE_PATH=${PKG_DIR}/include
+    export ACLOCAL_PATH=${PKG_DIR}/share/aclocal
+    export MONO_REGISTRY_PATH=~/.mono/registry
+fi
 
 #TODO: Use Paket project from https://fsprojects.github.io/Paket/index.html
-mono "src\.nuget\NuGet.exe" Install FAKE -OutputDirectory build -ExcludeVersion -Verbosity Detailed
-mono "src\.nuget\NuGet.exe" Install FSharp.Data -OutputDirectory build -ExcludeVersion -Verbosity Detailed
+mono "./src/.nuget/NuGet.exe" Install FAKE -OutputDirectory build -ExcludeVersion -Verbosity Detailed
+mono "./src/.nuget/NuGet.exe" Install FSharp.Data -OutputDirectory build -ExcludeVersion -Verbosity Detailed
 
 #.paket\paket.bootstrapper.exe
 #if errorlevel 1 (
@@ -27,6 +31,4 @@ mono "src\.nuget\NuGet.exe" Install FSharp.Data -OutputDirectory build -ExcludeV
 #  exit /b %errorlevel%
 #)
 
-#export MONO_PATH=${MONO_PATH}:${PWD}/build/FAKE/tools
-
-mono "build\FAKE\tools\Fake.exe" build.fsx $1 --logfile build.log
+mono "./build/FAKE/tools/Fake.exe" build.fsx $1 --logfile build.log
